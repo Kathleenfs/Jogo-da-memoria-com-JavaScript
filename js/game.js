63748cd781241fc77f359fbf6eb91863443deab1
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-08-31 20:42:04 -0300

    funções criadas em game
    endgame
    checkcards

--- js/game.js
+++ js/game.js
@@ -22,8 +22,39 @@ const createElement = (tag,className)=>{
 let firstCard = '';
 let secondCard = '';
 
-const checkCards = () =>{
+const checkEndGame = ()=>{
+    const disabledCards = document.querySelectorAll('.disabled-card');
 
+    if(disabledCards.length === 20){
+        alert('Parabens vc ganhou');
+    }
+}
+
+const checkCards = () =>{
+    const firstCharacter = firstCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+
+  if (firstCharacter === secondCharacter) {
+
+    firstCard.firstChild.classList.add('disabled-card');
+    secondCard.firstChild.classList.add('disabled-card');
+
+    firstCard = '';
+    secondCard = '';
+
+    checkEndGame();
+
+  } else {
+    setTimeout(() => {
+
+      firstCard.classList.remove('reveal-card');
+      secondCard.classList.remove('reveal-card');
+
+      firstCard = '';
+      secondCard = '';
+
+    }, 500);
+  }
 }
 
 const revealCard = ({ target }) =>{
