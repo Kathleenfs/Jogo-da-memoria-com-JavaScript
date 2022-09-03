ecfaaf50a87facf8718f3ea4b18e6a8fe7f56a94
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-09-03 13:47:54 -0300

    atualização formato img

--- js/game.js
+++ js/game.js
@@ -89,7 +89,7 @@ const createCard = (character) => {
   const front = createElement('div', 'face front');
   const back = createElement('div', 'face back');
 
-  front.style.backgroundImage = `url('../images/${character}.png')`;
+  front.style.backgroundImage = `url('../images/${character}.jpg')`;
 
   card.appendChild(front);
   card.appendChild(back);
