4bf78fa0e0639f22447cbaeff141f940e1a0469c
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-09-03 13:28:34 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ const grid = document.querySelector('.grid');
 const spanPlayer = document.querySelector('.player');
 const timer = document.querySelector('.timer');
 
+
 const characters= [
     'joel',
     'cama',
@@ -15,27 +16,27 @@ const characters= [
     'cj'
 ];
 
-const createElement = (tag,className)=>{
-    const element = document.createElement(tag);
-    element.className = className;
-    return element;
+const createElement = (tag, className) => {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
 }
 
 let firstCard = '';
 let secondCard = '';
 
-const checkEndGame = ()=>{
-    const disabledCards = document.querySelectorAll('.disabled-card');
+const checkEndGame = () => {
+  const disabledCards = document.querySelectorAll('.disabled-card');
 
-    if(disabledCards.length === 20){
-        clearInterval(this.loop)
-        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
-    }
+  if (disabledCards.length === 20) {
+    clearInterval(this.loop);
+    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
+  }
 }
 
-const checkCards = () =>{
-    const firstCharacter = firstCard.getAttribute('data-character');
-    const secondCharacter = secondCard.getAttribute('data-character');
+const checkCards = () => {
+  const firstCharacter = firstCard.getAttribute('data-character');
+  const secondCharacter = secondCard.getAttribute('data-character');
 
   if (firstCharacter === secondCharacter) {
 
@@ -58,66 +59,70 @@ const checkCards = () =>{
 
     }, 500);
   }
+
 }
 
-const revealCard = ({ target }) =>{
+const revealCard = ({ target }) => {
 
-    if(target.parentNode.className.includes('reaveal-card')){
-        return;
-    }
-    if(firstCard === ''){
+  if (target.parentNode.className.includes('reveal-card')) {
+    return;
+  }
 
-        target.parentNode.classList.add('reveal-card');
-        firstCard = target.parentNode;
-    }else if (secondCard === ''){
+  if (firstCard === '') {
 
-        target.parentNode.classList.add('reveal-card')
-        secondCard = target.parentNode;
+    target.parentNode.classList.add('reveal-card');
+    firstCard = target.parentNode;
 
-        checkCards();
-    }      
-    
+  } else if (secondCard === '') {
+
+    target.parentNode.classList.add('reveal-card');
+    secondCard = target.parentNode;
+
+    checkCards();
+
+  }  
 }
 
-const createCard = (character) =>{
+const createCard = (character) => {
 
-    const card = createElement('div', 'card');
-    const front = createElement('div','face front');
-    const back= createElement('div','face back');
+  const card = createElement('div', 'card');
+  const front = createElement('div', 'face front');
+  const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url('../images/${character}.jpg')`;
-    card.appendChild(front);
-    card.appendChild(back);
+  front.style.backgroundImage = `url('../images/${character}.png')`;
 
-    card.addEventListener('click', revealCard);
-    card.setAttribute('data-character', character);
+  card.appendChild(front);
+  card.appendChild(back);
 
-    return card;
+  card.addEventListener('click', revealCard);
+  card.setAttribute('data-character', character)
+
+  return card;
 }
 
 const loadGame = () => {
-    const duplicateCharacters = [...characters, ...characters];
+  const duplicateCharacters = [ ...characters, ...characters ];
 
-    const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+  const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
 
-    shuffledArray.forEach((character)=>{
-        const card = createCard(character);
-        grid.appendChild(card);
-    });
+  shuffledArray.forEach((character) => {
+    const card = createCard(character);
+    grid.appendChild(card);
+  });
 }
 
 const startTimer = () => {
 
-    this.loop = setInterval(() => {
-      const currentTime = +timer.innerHTML; //+ na frente convert string em number
-      timer.innerHTML = currentTime + 1;
-    }, 1000);
-  
-  }
-  
-  window.onload = () => {
-    spanPlayer.innerHTML = localStorage.getItem('player');
-    startTimer();
-    loadGame();
-  }
+  this.loop = setInterval(() => {
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1; // + na frente converte string em number
+  }, 1000);
+
+}
+
+window.onload = () => {
+  spanPlayer.innerHTML = localStorage.getItem('player');
+  startTimer();
+  loadGame();
+}
 
